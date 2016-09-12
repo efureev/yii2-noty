03c4d8448f1b77a9689789c5f71876a396e8c527
efureev <efureev@yandex.ru>
2016-09-12 18:08:22 +0500

    fix

---
+++ tests/unit/noty/MainTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace efureev\tests\unit\noty;
+
+use yii\web\View;
+
+/**
+ * Class MainTest
+ *
+ * @package efureev\tests\unit\noty
+ */
+class MainTest extends \efureev\tests\unit\TestCase
+{
+
+    public function testMain()
+    {
+
+    }
+
+}
